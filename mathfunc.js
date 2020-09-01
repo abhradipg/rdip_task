973e93784e190371466d3d5b5cf31e826bfbb4d1
abhradipg <abhradip.ghosh9@gmail.com>
2020-09-01 21:12:27 +0530

    math functions

---
+++ mathfunc.js
@@ -0,0 +1,14 @@
+var x=4;
+var y=2;  
+ 
+console.log(Math.pow(x,y));
+console.log(Math.sqrt(x));
+
+y=-7.8;
+
+console.log(Math.abs(y));
+console.log(Math.ceil(y));
+console.log(Math.floor(y));
+
+y=Math.random();
+console.log(Math.sin(y));
